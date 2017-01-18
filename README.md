4742b1ac51ec3ea60dadc4b0d38bd21f2efe82f0
sergiomgaspar <sergiomgaspar.dev@gmail.com>
2017-01-18 18:06:39 +0100

    final version fully featured

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# smg-req-parser
+# Request Header Parser Microservice
+
+This api was created for the FreeCodeCamp exercice "Request Header Parser Microservice"
+
+* [FreeCodeCamp] - Link to the exercice
+* [Heroku] - Online deployed version
+
+### API description
+
+The Request Header Parser API must return the IP Address, Language code and Software description present in the header of the HTTP request.
+
+  - **Use-case 1:** API returns the IP address, language and operating system for the requester browser.
+
+## Example (usage)
+Use the URL below to get the date in the desired format
+```sh
+https://smg-req-parser.herokuapp.com/
+```
+## Example (result)
+Below is shown the result returned from the URL above.
+```sh
+{"ipaddress": "75.748.86.91",
+  "language": "pt-PT",
+  "software": "OS: Windows 10.0 || Browser: Chrome || Platform: Microsoft Windows"}
+```
+***special kudos** if you recognize the address*
+## Local Installation
+
+You'll need to have the latest verison of node.js installed. Either use your OS's package manager or follow the installation instructions on the [official website](http://nodejs.org).
+
+Next, [install git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git) if it is not already installed. To clone this repository to your local machine, open a command line interface and navigate to your projects directory. Then type
+
+`$ git clone https://github.com/sergiomgaspar/smg-req-parser.git`
+
+Move to the `smg-req-parser` subdirectory and type `npm install`. This installs all of the API dependencies.
+
+Finally, type `npm start` to start the application. If all goes well, it will be available at `http://localhost:3000`.
+
+
+## Technologies used
+This is a very small example of an API created in NodeJS using the Express Framework.
+
+## License
+
+[![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png)](http://creativecommons.org/publicdomain/zero/1.0/)
+
+To the extent possible under law, the author has waived all copyright and related or neighboring rights to this work.
+
+[FreeCodeCamp]: <https://www.freecodecamp.com/challenges/request-header-parser-microservice>
+[Heroku]: <https://smg-req-parser.herokuapp.com/>
--- req_parser.js
+++ req_parser.js
@@ -1,27 +1,24 @@
 /* 
-	Timestamp Microservice for FreeCodeCamp
+	Request Header Parser Microservice for FreeCodeCamp
 	by: Sergio Gaspar
-	date: 2017/01/17
+	date: 2017/01/18
 	
-	Create miscroservice with the below user stories:
+	Create miscroservice with the below user storie:
 	
-	US1: I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016).
-	US2: If it does, it returns both the Unix timestamp and the natural language form of that date.
-	US3: If it does not contain a date or Unix timestamp, it returns null for those properties.User
+	US1: I can get the IP address, language and operating system for my browser.
 */
 var express = require('express');
-//var fs = require('fs');
-var url = require('url');
+var useragent = require('express-useragent'); // npm install express-useragent --save 
 var app = express();
 var port = process.env.PORT || 3000;
-var isDebug = true;
+var isDebug = false;
 
 // Simple Log function to print to console
-function Log(level, logStr){
-	if (isDebug && level === 'D') console.log("DEBUG: "+logStr);
-		else if (level === 'I') console.log("INFO: "+logStr);
-			else if (level === 'E') console.log("ERROR: "+logStr);
-				else console.log(level+": "+logStr);
+function Log(level, logStr) {
+	if (isDebug && level === 'D') console.log("DEBUG: " + logStr);
+	else if (level === 'I') console.log("INFO: " + logStr);
+	else if (level === 'E') console.log("ERROR: " + logStr);
+	else console.log(level + ": " + logStr);
 };
 
 // Handle the site icon request... return nothing
@@ -33,32 +30,39 @@ app.get('/favicon.ico', function(req, res) {
 // Express app handles all GET regardless of URL (no routing)
 app.get('/', function(req, res) {
 
-	//var ip = res.connection.remoteAddress;
-	var ip = req.connection.remoteAddress ||
-		req.headers['x-forwarded-for'] || 
+	// Get the IP address from the request header using the req object directly (may be a better way using FW)
+	var ip = req.headers['x-forwarded-for'] ||
+		req.connection.remoteAddress ||
 		req.socket.remoteAddress ||
 		req.connection.socket.remoteAddress;
-	Log('D','IP: '+ip);
 
-	/*
-		example:
-		"ipaddress": "46.140.43.46",
-		"language": "pt-PT",
-		"software": "Windows NT 10.0; Win64; x64"
-	*/
+	Log('D', 'IP: ' + ip);
+
+	// Get the first language code from the request (again, may be a better way using FW)
+	var lang = req.headers["accept-language"];
+	lang = lang.substring(0, lang.indexOf(","));
+
+	Log('D', 'LANG: ' + lang);
+
+	// Get Software using express-useragent FW to make it "prettier"
+	var ua = useragent.parse(req.headers['user-agent']);
+	var soft = "OS: " + ua.os + " || Browser: " + ua.browser + " || Platform: " + ua.platform;
+
+	Log('D', 'SOFT: ' + soft);
+
+	// Write JSON object to result object
 	res.writeHead(200);
 	res.write(JSON.stringify({
 		"ipaddress": ip,
-		"language": null,
-		"software": null
+		"language": lang,
+		"software": soft
 	}));
 	res.end();
-	
+
 });
 
 // Listen on port 3000 by default, IP defaults to 127.0.0.1
 app.listen(port);
 
 // Logs port it is running on
-Log('I','Server running at http://127.0.0.1:' + port + '/');
-// URL to execute:  http://npm.sergiomgaspar.c9users.io/
+Log('I', 'Server running at http://127.0.0.1:' + port + '/');
